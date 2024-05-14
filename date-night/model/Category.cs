994548b4880afafda76b850175233c0651c683d2
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-14 13:02:30 +0400

    feat: category model

---
+++ date-night/model/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace date_night.model
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
