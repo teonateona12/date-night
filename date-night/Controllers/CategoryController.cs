198705f4313149248eb1017983239c398944d385
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-14 18:40:07 +0400

    feat: get categories methot

---
+++ date-night/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using date_night.model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace date_night.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Category>>> GetCategories()
+        {
+            var categories = new List<Category>
+            {
+                new Category { Id = 1, Image="", Title="kabebi"}
+            };
+            return Ok(categories);
+        }
+    }
+}
