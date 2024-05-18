e0dac0a75bae58e961dc342b94e011db531a2be9
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-18 15:29:07 +0400

    feat: add admin itema modal

--- date-night-admin/Data/DataContext.cs
+++ date-night-admin/Data/DataContext.cs
@@ -10,5 +10,11 @@ namespace date_night_admin.Data
 
         }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
--- date-night-admin/Model/Category.cs
+++ date-night-admin/Model/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace date_night_admin.Model
+﻿using Microsoft.Extensions.Hosting;
+
+namespace date_night_admin.Model
 {
     public class Category
     {
         public int Id { get; set; }
         public string Title { get; set; }
         public string Image { get; set; }
+        public ICollection<Item> Items { get; set; }
     }
 }
