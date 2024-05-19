d177c0a7c7ceea61f0687306ad1ed8171f2521c3
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-19 14:07:09 +0400

    feat: admin get items method

--- date-night-admin/Controllers/ItemController.cs
+++ date-night-admin/Controllers/ItemController.cs
@@ -8,6 +8,19 @@ namespace date_night_admin.Controllers
     [ApiController]
     public class ItemController : ControllerBase
     {
-       
+        private readonly IItemRepository itemRepository;
+
+        public ItemController(IItemRepository itemRepository)
+        {
+            this.itemRepository = itemRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Item>>> GetItems()
+        {
+            var items = await itemRepository.GetAllAsync();
+
+            return Ok(items);
+        }
     }
 }
