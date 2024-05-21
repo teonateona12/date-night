b324fa9898b03d2503e316c83e321ece7debfa1e
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-21 17:27:21 +0400

    feat: admin configure automaper profile

---
+++ date-night-admin/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using date_night_admin.Model;
+
+namespace date_night_admin
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Item, ItemDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+        }
+    }
+}
