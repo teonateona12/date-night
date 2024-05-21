1ccaeea8701039369f0c0d3ccf32592a62988e3c
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-21 17:25:18 +0400

    feat: admin create category and item dto

---
+++ date-night-admin/Model/CategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace date_night_admin.Model
+{
+    public class CategoryDto
+    {
+        public string Title { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ date-night-admin/Model/ItemDto.cs
@@ -0,0 +1,9 @@
+﻿namespace date_night_admin.Model
+{
+    public class ItemDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
