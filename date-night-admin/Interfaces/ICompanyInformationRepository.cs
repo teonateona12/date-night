a936b7f18f094959a2ad1a8e5a4c9abe73959a0f
teonateona12 <teona.piranishvili1@gmail.com>
2024-05-31 19:16:32 +0400

    feat: admin - create company information interface

---
+++ date-night-admin/Interfaces/ICompanyInformationRepository.cs
@@ -0,0 +1,13 @@
+﻿using date_night_admin.Model;
+
+namespace date_night_admin.Interfaces
+{
+    public interface ICompanyInformationRepository
+    {
+        Task<List<CompanyInformation>> GetAll();
+        Task<CompanyInformation> Delete(int id);
+        Task<CompanyInformation> Update(int id, CompanyInformation companyInformation);
+        Task<CompanyInformation> Create(CompanyInformation companyInformation);
+
+    }
+}
