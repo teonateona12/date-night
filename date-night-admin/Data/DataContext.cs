5a3337d37a30ab62dfbb05f2aea7689e9db11beb
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-05 18:23:42 +0400

    feat: admin - create about company database

--- date-night-admin/Data/DataContext.cs
+++ date-night-admin/Data/DataContext.cs
@@ -12,6 +12,7 @@ namespace date_night_admin.Data
         public DbSet<Category> Categories { get; set; }
         public DbSet<Item> Items { get; set; }
         public DbSet<CompanyInformation> CompanyInformation { get; set; }
+        public DbSet<AboutCompany> AboutCompany { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
