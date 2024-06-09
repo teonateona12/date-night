add2b51d6da9be1a16e829b9b4a94566ba136477
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-10 00:11:32 +0400

    feat: user - get about company repository

---
+++ date-night/Repository/AboutCompanyRepository.cs
@@ -0,0 +1,25 @@
+﻿using date_night_user.Data;
+using date_night_user.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace date_night_user.Repository
+{
+    public class AboutCompanyRepository : IAboutCompanyRepository
+    {
+        private readonly DataContext context;
+
+        public AboutCompanyRepository(DataContext context)
+        {
+            this.context = context;
+        }
+        public async Task<AboutCompany> GetAsync()
+        {
+            var company = await context.AboutCompany.FirstOrDefaultAsync();
+            if (company == null)
+            {
+                return null;
+            }
+            return company;
+        }
+    }
+}
