0dae8a29b4a35df87b5ef5f999af97e50576a8fb
teonateona12 <teona.piranishvili1@gmail.com>
2024-06-10 00:15:19 +0400

    feat: user - get about company controller

---
+++ date-night/Controllers/AboutCompanyController.cs
@@ -0,0 +1,30 @@
+﻿using date_night_user.Data;
+using date_night_user.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace date_night_user.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutCompanyController : ControllerBase
+    {
+        private readonly IAboutCompanyRepository aboutCompany;
+
+        public AboutCompanyController(IAboutCompanyRepository aboutCompany)
+        {
+            this.aboutCompany = aboutCompany;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<AboutCompany>> Get()
+        {
+            var company = await aboutCompany.GetAsync();
+            if(company == null)
+            {
+                return NotFound();
+            }
+            return Ok(company);
+        }
+    }
+}
